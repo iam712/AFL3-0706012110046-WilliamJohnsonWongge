5f91f13fccf2ca56911277d5a232504916cbb147
iam712 <94553464+iam712@users.noreply.github.com>
2023-04-15 01:30:31 +0700

    Chapter 3 Section 6

---
+++ AFL3-0706012110046-WilliamJohnsonWongge/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,29 @@
+//
+//  FavoriteButton.swift
+//  AFL3-0706012110046-WilliamJohnsonWongge
+//
+//  Created by William j. Wongge on 15/04/23.
+//
+
+import SwiftUI
+
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
