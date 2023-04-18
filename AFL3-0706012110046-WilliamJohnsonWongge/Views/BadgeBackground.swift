c9f899f8e016cf1ee59854797fdb4a801686cedf
iam712 <94553464+iam712@users.noreply.github.com>
2023-04-18 08:35:57 +0700

    Chapter 4 Section 2 (1)

--- AFL3-0706012110046-WilliamJohnsonWongge/Views/BadgeBackground.swift
+++ AFL3-0706012110046-WilliamJohnsonWongge/Views/BadgeBackground.swift
@@ -11,7 +11,7 @@ struct BadgeBackground: View {
     var body: some View {
         GeometryReader { geometry in
             Path { path in
-                var width: CGFloat = 100.0
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
                 let height = width
                 let xScale: CGFloat = 0.832
                 let xOffset = (width * (1.0 - xScale)) / 2.0
@@ -22,7 +22,7 @@ struct BadgeBackground: View {
                         y: height * (0.20 + HexagonParameters.adjustment)
                     )
                 )
-                
+
                 HexagonParameters.segments.forEach { segment in
                     path.addLine(
                         to: CGPoint(
@@ -30,7 +30,7 @@ struct BadgeBackground: View {
                             y: height * segment.line.y
                         )
                     )
-                    
+
                     path.addQuadCurve(
                         to: CGPoint(
                             x: width * segment.curve.x + xOffset,
@@ -43,14 +43,12 @@ struct BadgeBackground: View {
                     )
                 }
             }
-            
             .fill(.linearGradient(
                 Gradient(colors: [Self.gradientStart, Self.gradientEnd]),
                 startPoint: UnitPoint(x: 0.5, y: 0),
                 endPoint: UnitPoint(x: 0.5, y: 0.6)
             ))
         }
-        .aspectRatio(1, contentMode: .fit)
     }
     static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
     static let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
