17e6d4427c274e09a5de1d663d58a96d82094fa9
iam712 <94553464+iam712@users.noreply.github.com>
2023-04-24 12:14:05 +0700

    Chapter 6 Section 1

---
+++ AFL3-0706012110046-WilliamJohnsonWongge/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  AFL3-0706012110046-WilliamJohnsonWongge
+//
+//  Created by William j. Wongge on 24/04/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView{
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
