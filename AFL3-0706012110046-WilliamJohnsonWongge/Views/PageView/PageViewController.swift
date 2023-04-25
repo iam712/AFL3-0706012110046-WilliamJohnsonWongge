7b77ba34e412db1aba75982a4b2f688c0d470db0
iam712 <94553464+iam712@users.noreply.github.com>
2023-04-25 13:02:17 +0700

    Chapter 8 Section 1

---
+++ AFL3-0706012110046-WilliamJohnsonWongge/Views/PageView/PageViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PageViewController.swift
+//  AFL3-0706012110046-WilliamJohnsonWongge
+//
+//  Created by William j. Wongge on 25/04/23.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+        
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+    }
+}
